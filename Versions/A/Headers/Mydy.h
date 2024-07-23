8f4d4bdd077ce159b947493f138d872172403499
baihongfei <baihongfei@baihongfeideMac.local>
2024-07-23 11:35:21 +0800

    a

---
+++ Versions/A/Headers/Mydy.h
@@ -0,0 +1,18 @@
+//
+//  Mydy.h
+//  Mydy
+//
+//  Created by baihongfei on 2024/7/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mydy.
+FOUNDATION_EXPORT double MydyVersionNumber;
+
+//! Project version string for Mydy.
+FOUNDATION_EXPORT const unsigned char MydyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mydy/PublicHeader.h>
+
+
